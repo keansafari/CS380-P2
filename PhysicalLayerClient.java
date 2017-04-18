ba08a1dde7370b74fd75d8f6a29ab9be489d4abb
Kean Jafari <khjafari@cpp.edu>
2017-04-18 16:12:25 -0700

    added hashmap to create conversionn table

--- PhysicalLayerClient.java
+++ PhysicalLayerClient.java
@@ -1,22 +1,28 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import java.util.zip.CRC32;
 import java.nio.*;
 import java.lang.String;
 import java.lang.*;
+import java.text.*;
 
 public class PhysicalLayerClient {
+
 
 	public static void main(String[] args) {
 
 		try(Socket socket = new Socket("codebank.xyz", 38002)) {
 			System.out.println("\nConnected to Server.");
+
+			//Create Conversion map since is connection successful
+			HashMap convmap = create4b5bconversionmap();
 			
 			//Get Baseline - read 64 bits and average them
 			double averageBaseline = getBaseline(socket);
 			System.out.println("Baseline established from preamble: " + averageBaseline);
 			
+
+
 			//Get and Print 32 bytes
 			//byte[] nzri = getNZRI();
 			
@@ -43,6 +49,8 @@ public class PhysicalLayerClient {
 				averageBaseline += baseline[i];
 			}
 			averageBaseline = averageBaseline / 64;
+			DecimalFormat decimal = new DecimalFormat("#.##");
+			averageBaseline = Double.valueOf(decimal.format(averageBaseline));
 			return averageBaseline;
 
 
@@ -59,4 +67,27 @@ public class PhysicalLayerClient {
 		}
 		
 	}*/
+
+
+	private static HashMap create4b5bconversionmap() {
+		HashMap<String, String> map = new HashMap<String, String>() {{
+			put("11110", "0000"); 
+			put("10010", "1000");
+	        put("01001", "0001"); 
+	        put("10011", "1001");
+	        put("10100", "0010"); 
+	        put("10110", "1010");
+	        put("10101", "0011"); 
+	        put("10111", "1011");
+	        put("01010", "0100"); 
+	        put("11010", "1100");
+	        put("01011", "0101"); 
+	        put("11011", "1101");
+	        put("01110", "0110"); 
+	        put("11100", "1110");
+	        put("01111", "0111"); 
+	        put("11101", "1111");
+	    }};
+	    return map;
+	}
 }
